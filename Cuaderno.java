513c0fb090eaa5532985c2f850fcaea97a6a55d4
JoacoGuardia <joacoguardia10@gmail.com>
2023-05-06 22:49:43 -0300

    Actualizacion Cuaderno

--- Cuaderno.java
+++ Cuaderno.java
@@ -1,6 +1,18 @@
 import javax.swing.*;
 
 public class Cuaderno extends JFrame{
+    public Cuaderno(){
+        setSize(1000, 700);
+        setTitle("Cuaderno de Comunicaciones Digital");
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public void Inicio(){
+        
+    }
+    public void InicioSesion(){}
+    public void Registro(){}
     public void MenuAlumno(){}
     public void MenuAdultoResponsable(){}
     public void MenuDocenteJardin(){}
